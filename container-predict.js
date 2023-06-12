590f77e38de221abc726aab9b4cb3b157455f7a7
Pantoja14022020 <tl419411@uaeh.edu.mx>
2023-06-12 17:24:33 -0600

    Predictor EC FrontEnd. Version 1

---
+++ container-predict.js
@@ -0,0 +1,120 @@
+const questionContainer = document.querySelector('#questions-container')
+
+const diabetesContainerOption = document.querySelector('.diabetes');
+const hipertensionContainerOption = document.querySelector('.hipertension');
+const erContainerOption = document.querySelector('.er');
+
+const backgroundHeader = document.querySelector('#background-header');//Seleccino el contenedor que cambia el color de fondo en el header
+const formCont = document.querySelector('#form-questions');//Selecciono el formulario que contiene  las preguntas
+const edadInput = document.querySelector('#edad-input');
+const spinner = document.getElementById('spinner-container');
+
+diabetesContainerOption.addEventListener('click',()=>{
+    if(!questionContainer.classList.contains('hide')){
+        questionContainer.classList.add('hide');
+    }
+    spinner.classList.remove('hide');
+    setTimeout(()=>{
+        spinner.classList.add('hide');
+        if(questionContainer.classList.contains('hide')){
+            questionContainer.classList.remove('hide');
+        }
+        if(activo == 0) {
+            activo=1;
+            questionsContainer[activo].classList.add('hide-question')
+            edadInput.classList.remove('hide-question');
+            activo = 0;
+            inputEdad.value = "";
+            btn.classList.add('hide-question')
+        }
+        if(activo > 0 || activo != 1){
+            if(questionsContainer[activo+1]){
+                questionsContainer[activo+1].classList.add('hide-question')
+                edadInput.classList.remove('hide-question');
+                activo = 0;
+                inputEdad.value = "";
+                btn.classList.add('hide-question')
+                btnAnalizar.classList.add('hide-question');
+            }
+        }
+    
+        backgroundHeader.innerHTML = "";
+        backgroundHeader.innerHTML = "<svg id=\"visual\" viewBox=\"0 0 2000 150\" width=\"2000\" height=\"150\" xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" version=\"1.1\"><rect x=\"0\" y=\"0\" width=\"2000\" height=\"150\" fill=\"#C62368\"></rect><path d=\"M0 60L47.7 65.8C95.3 71.7 190.7 83.3 285.8 90.3C381 97.3 476 99.7 571.2 96.3C666.3 93 761.7 84 857 85.7C952.3 87.3 1047.7 99.7 1143 101C1238.3 102.3 1333.7 92.7 1428.8 91C1524 89.3 1619 95.7 1714.2 98.5C1809.3 101.3 1904.7 100.7 1952.3 100.3L2000 100L2000 151L1952.3 151C1904.7 151 1809.3 151 1714.2 151C1619 151 1524 151 1428.8 151C1333.7 151 1238.3 151 1143 151C1047.7 151 952.3 151 857 151C761.7 151 666.3 151 571.2 151C476 151 381 151 285.8 151C190.7 151 95.3 151 47.7 151L0 151Z\" fill=\"#f23882\" stroke-linecap=\"round\" stroke-linejoin=\"miter\"></path></svg>";
+    },500)
+    enfermedadApredecir = "diabetes";
+});
+hipertensionContainerOption.addEventListener('click',()=>{
+    if(!questionContainer.classList.contains('hide')){
+        questionContainer.classList.add('hide');
+    }
+    spinner.classList.remove('hide');
+
+    setTimeout(()=>{
+        spinner.classList.add('hide');
+        
+        if(questionContainer.classList.contains('hide')){
+            questionContainer.classList.remove('hide');
+        }
+        if(activo == 0) {
+            activo=1;
+            questionsContainer[activo].classList.add('hide-question')
+            edadInput.classList.remove('hide-question');
+            activo = 0;
+            inputEdad.value = "";
+            btn.classList.add('hide-question')
+        }
+        if(activo > 0 || activo != 1){
+            if(questionsContainer[activo+1]){
+                questionsContainer[activo+1].classList.add('hide-question')
+                edadInput.classList.remove('hide-question');
+                activo = 0;
+                inputEdad.value = "";
+                btn.classList.add('hide-question')
+                btnAnalizar.classList.add('hide-question');
+            }
+        }
+    
+        backgroundHeader.innerHTML = "";
+        backgroundHeader.innerHTML = "<svg id=\"visual\" viewBox=\"0 0 2000 150\" width=\"2000\" height=\"150\" xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" version=\"1.1\"><rect x=\"0\" y=\"0\" width=\"2000\" height=\"150\" fill=\"#F7770F\"></rect><path d=\"M0 148L47.7 136C95.3 124 190.7 100 285.8 90.3C381 80.7 476 85.3 571.2 84.2C666.3 83 761.7 76 857 77.5C952.3 79 1047.7 89 1143 93C1238.3 97 1333.7 95 1428.8 87C1524 79 1619 65 1714.2 70C1809.3 75 1904.7 99 1952.3 111L2000 123L2000 151L1952.3 151C1904.7 151 1809.3 151 1714.2 151C1619 151 1524 151 1428.8 151C1333.7 151 1238.3 151 1143 151C1047.7 151 952.3 151 857 151C761.7 151 666.3 151 571.2 151C476 151 381 151 285.8 151C190.7 151 95.3 151 47.7 151L0 151Z\" fill=\"#ff9f0e\" stroke-linecap=\"round\" stroke-linejoin=\"miter\"></path></svg>";
+
+    },500)
+    enfermedadApredecir = "hipertension";
+});
+erContainerOption.addEventListener('click',()=>{
+
+    if(!questionContainer.classList.contains('hide')){
+        questionContainer.classList.add('hide');
+    }
+    spinner.classList.remove('hide');
+
+    setTimeout(()=>{
+        spinner.classList.add('hide');
+
+        if(questionContainer.classList.contains('hide')){
+            questionContainer.classList.remove('hide');
+        }
+
+        if(activo == 0) {
+            activo=1;
+            questionsContainer[activo].classList.add('hide-question')
+            edadInput.classList.remove('hide-question');
+            activo = 0;
+            inputEdad.value = "";
+            btn.classList.add('hide-question')
+        }
+        if(activo > 0 || activo != 1){
+            if(questionsContainer[activo+1]){
+                questionsContainer[activo+1].classList.add('hide-question')
+                edadInput.classList.remove('hide-question');
+                activo = 0;
+                inputEdad.value = "";
+                btn.classList.add('hide-question')
+                btnAnalizar.classList.add('hide-question');
+            }
+        }
+
+        backgroundHeader.innerHTML = "";
+        backgroundHeader.innerHTML = "<svg id=\"visual\" viewBox=\"0 0 2000 150\" width=\"2000\" height=\"150\" xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" version=\"1.1\"><rect x=\"0\" y=\"0\" width=\"2000\" height=\"150\" fill=\"#135aff\"></rect><path d=\"M0 67L47.7 79.7C95.3 92.3 190.7 117.7 285.8 125.3C381 133 476 123 571.2 113.8C666.3 104.7 761.7 96.3 857 88.3C952.3 80.3 1047.7 72.7 1143 70.8C1238.3 69 1333.7 73 1428.8 70.3C1524 67.7 1619 58.3 1714.2 62.2C1809.3 66 1904.7 83 1952.3 91.5L2000 100L2000 151L1952.3 151C1904.7 151 1809.3 151 1714.2 151C1619 151 1524 151 1428.8 151C1333.7 151 1238.3 151 1143 151C1047.7 151 952.3 151 857 151C761.7 151 666.3 151 571.2 151C476 151 381 151 285.8 151C190.7 151 95.3 151 47.7 151L0 151Z\" fill=\"#4c82ff\" stroke-linecap=\"round\" stroke-linejoin=\"miter\"></path></svg>";
+    },500)
+    enfermedadApredecir = "er";
+});
---
+++ questions.js
@@ -0,0 +1,223 @@
+
+
+const btn = document.getElementById("btn");
+const inputEdad =  document.getElementById('edad');
+const inputEdadTxt = document.querySelector('.edad-input-txt');
+
+const btnAnalizar =  document.getElementById('btn-analizar');
+const btnReiniciar =  document.getElementById('btn-reiniciar');
+
+
+const questionsContainer = document.getElementsByClassName('question')
+
+var activo;
+
+
+inputEdad.addEventListener('input',e=>{
+    if(e.target.value != ""){
+        if(btn.classList.contains('hide-question')){
+            btn.classList.remove('hide-question')
+        }
+    }else{
+        if(!btn.classList.contains('hide-question')){
+            btn.classList.add('hide-question')
+        }
+    }
+})
+
+
+const messageInputTxtEdad = document.querySelector('#message-warning');
+
+btn.addEventListener('click',e=>{
+    let txtEdad = parseInt(inputEdadTxt.value);
+    if(txtEdad < 18 || txtEdad >= 100){
+        messageInputTxtEdad.innerHTML = "";
+        messageInputTxtEdad.innerHTML = "La edad minima es 18 años";
+        messageInputTxtEdad.classList.remove('show-message');
+        setTimeout(()=>{
+            messageInputTxtEdad.classList.add('show-message');
+        },2000);
+        return;
+    }
+
+    if(activo != 20){//21
+        for(let i = 0; i < questionsContainer.length; i++){
+            if(!questionsContainer[i].classList.contains('hide-question')){
+                activo = i;
+            }
+        } 
+        questionsContainer[activo].classList.add('hide-question')
+        questionsContainer[activo+1].classList.remove('hide-question') 
+    }
+    if(activo==20){//21
+        btn.classList.add('hide-question')
+        btnAnalizar.classList.remove('hide-question')       
+    }
+    
+})
+
+
+
+btnAnalizar.addEventListener('click',e=>{
+
+    questionsContainer[questionsContainer.length-1].classList.add('hide-question')
+
+    btnAnalizar.classList.add('hide-question')
+
+    const i1 = parseInt( document.getElementById("edad").value);
+    const i2 = parseInt(document.getElementsByClassName("2_i")[0].value);
+    const i3 = parseInt(document.getElementsByClassName("3_i")[0].value);
+    const i4 = parseInt(document.getElementsByClassName("4_i")[0].value);
+    const i5 = parseInt(document.getElementsByClassName("5_i")[0].value);
+    const i6 = parseInt(document.getElementsByClassName("6_i")[0].value);
+    const i7 = parseInt(document.getElementsByClassName("7_i")[0].value);
+    const i8 = parseInt(document.getElementsByClassName("8_i")[0].value);
+    const i9 = parseInt(document.getElementsByClassName("9_i")[0].value);
+    const i10 = parseInt(document.getElementsByClassName("10_i")[0].value);
+    const i11 = parseInt(document.getElementsByClassName("11_i")[0].value);
+    const i12 = parseInt(document.getElementsByClassName("12_i")[0].value);
+    const i13 = parseInt(document.getElementsByClassName("13_i")[0].value);
+    const i14 = parseInt(document.getElementsByClassName("14_i")[0].value);
+    const i15 = parseInt(document.getElementsByClassName("15_i")[0].value);
+    const i16 = parseInt(document.getElementsByClassName("16_i")[0].value);
+    const i17 = parseInt(document.getElementsByClassName("17_i")[0].value);
+    const i18 = parseInt(document.getElementsByClassName("18_i")[0].value);
+    const i19 = parseInt(document.getElementsByClassName("19_i")[0].value);
+    const i20 = parseInt(document.getElementsByClassName("20_i")[0].value);
+    const i21 = parseInt(document.getElementsByClassName("21_i")[0].value);
+    const i22 = parseInt(document.getElementsByClassName("22_i")[0].value);
+
+    let entradas = {
+        "edad": i1,
+        "genero": i2,
+        "ec": i3,
+        "ocupacion": i4,
+        "tresd": i5,
+        "sefvr": i6,
+        "ecaaqla": i7,
+        "cvcspeu": i8,
+        "uoafpc": i9,
+        "ecarh": i10,
+        "eccoccl": i11,
+        "ecuoaf": i12,
+        "esfhacap": i13,
+        "oapb": i14,
+        "oapc": i15,
+        "ecraca": i16,
+        "cqvuve": i17,
+        "saonacctet": i18,
+        "ccsa": i19,
+        "hpisa": i20,
+        "scmn": i21,
+        "seacr": i22,
+    }
+
+    let queUrl = "";
+    let queImg = "";
+
+    if(enfermedadApredecir === "diabetes"){
+        queUrl = "https://api-ec-django.onrender.com/predecirDiabetes/";
+        queImg = "<img src=\"./assets/diabetes.png\" alt=\"diabetes\" width=\"100%\" height=\"30%\" />";
+    }else if(enfermedadApredecir === "hipertension"){
+        queUrl = "https://api-ec-django.onrender.com/predecirHipertension/";
+        queImg = "<img src=\"./assets/hipertension.png\" alt=\"hipertension\" width=\"100%\" height=\"30%\" />";
+    }else{//er enfermedad respiratoria
+        queUrl = "https://api-ec-django.onrender.com/predecirER/";
+        queImg = "<img src=\"./assets/er.png\" alt=\"er\" width=\"100%\" height=\"30%\" />";
+    }
+
+    //Mostrar el icono de spinner
+    spinner.classList.remove('hide');
+    
+    //Mostrar mi el boton de reiniciar para comenzar de nuevo
+    //btnReiniciar.classList.remove('hide-question')
+
+    //convertimos a string el json que contienen las entradas
+    entradas = JSON.stringify(entradas)
+
+    $.ajax({
+        url: queUrl,
+        type: 'POST',
+        data: entradas,
+        success: function({prediccion}) {
+          
+          let p = "";
+
+          if(prediccion == 1){
+            p = "<p>Positivo</p>";
+          }else{
+            p = "<p>Negativo</p>";
+          }
+
+          spinner.classList.add('hide');//Esconder el spinner
+          const modal = document.querySelector('#modal');
+          const details = document.querySelector('#details');
+
+          details.innerHTML = "";
+          details.innerHTML = queImg + p;
+
+          modal.classList.remove('hide-results');
+        },
+        error: function(xhr, status, error) {
+          console.log("No se pudo hacer la peticion")
+        }
+      });
+
+    /*
+    async function postData(url, data) {
+        const response = await fetch(url, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(data)
+        });
+        return response.json();
+    }
+
+    postData("http://127.0.0.1:5000/predecir", entradas)
+        .then( datos => {
+
+            let clase_predicha = datos.clase_predicha
+            let resultados = datos.resultados
+
+            const ctx = document.getElementById('myChart');
+
+            const data = {
+                labels: ["Ninguna", "Diabetes", "Hipertensión Arterial", "Enfermedad Respiratoria"],
+                datasets: [
+                {
+                    data: [(resultados[0]*100).toFixed(2), (resultados[1]*100).toFixed(2), (resultados[2]*100).toFixed(2), (resultados[3]*100).toFixed(2)],
+                    backgroundColor: ["#ff6384", "#36a2eb", "#ffce56", "#4bc0c0"],
+                },
+                ],
+            };
+
+            const options = {
+                responsive: true,
+                maintainAspectRatio: false,
+                legend: {
+                position: "bottom",
+                labels: {
+                    fontColor: "#333",
+                    fontSize: 14,
+                    boxWidth: 15,
+                    usePointStyle: true,
+                },
+                },
+            };
+            
+            new Chart(ctx, {
+                type: "pie",
+                data: data,
+                options: options,
+            });
+
+        })
+        .catch(error => console.error(error));*/
+})
+
+
+btnReiniciar.addEventListener('click',e=>{
+    window.location.href = "http://127.0.0.1:5500/";
+})    
